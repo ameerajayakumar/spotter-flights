4d839a0cf1feeb4da64431d94229efab15f9b813
Ameera Jayakumar <ameeraj.mec@gmail.com>
2025-02-06 00:03:41 +0200

    select components

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,5 @@
 // import { useState } from 'react';
-import { Autocomplete, Box, Button, Stack, TextField } from '@mui/material';
+import { Autocomplete, Box, Button, FormControl, InputLabel, MenuItem, Select, Stack, TextField, Typography } from '@mui/material';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
@@ -19,83 +19,133 @@ const Search = () => {
     { label: 'Pulp Fiction', year: 1994 },
   ];
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} height="100vh">
+    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} height="85vh">
+      <Typography variant="h3" color="#739fff" sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mb: { xs: 4, md: 10 } }}>
+        GoFlights
+      </Typography>
       <Stack
         minWidth="50%"
         maxWidth="90%"
         sx={{
-          justifyContent: 'center',
-          alignItems: 'center',
+          justifyContent: { xs: 'flex-start', md: 'center' },
+          alignItems: { xs: 'center', md: 'center' },
           bgcolor: '#fff',
           px: { xs: 3, sm: 5 },
           py: { xs: 2, sm: 3 },
           borderRadius: '7px',
-          flexDirection: 'column',
+          flexDirection: { xs: 'column' },
           gap: 2,
         }}
       >
         <Stack
-          minWidth="50%"
-          maxWidth="90%"
+          //   minWidth="50%"
+          //   maxWidth="90%"
           sx={{
-            justifyContent: 'flex-start',
-            alignItems: { xs: 'flex-start', md: 'center' },
-            // bgcolor: '#fff',
-            // px: { xs: 3, sm: 5 },
-            // py: { xs: 2, sm: 3 },
-            // borderRadius: '7px',
-            flexDirection: { xs: 'column', md: 'row' },
+            justifyContent: { xs: 'flex-start', md: 'flex-start' },
+            alignItems: { xs: 'flex-start', md: 'flex-start' },
+            bgcolor: '#fff',
+            flexDirection: { xs: 'column' },
             gap: 2,
           }}
         >
-          <Autocomplete
-            disablePortal
-            options={top100Films}
-            sx={{ width: { xs: '70%', sm: 300 } }}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                placeholder="Where from?"
-                label="Origin"
-                slotProps={{
-                  input: {
-                    ...params.InputProps,
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <FlightTakeoffIcon />
-                      </InputAdornment>
-                    ),
-                  },
-                }}
-              />
-            )}
-          />
-          <Autocomplete
-            disablePortal
-            options={top100Films}
-            sx={{ width: { xs: '70%', sm: 300 } }}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                placeholder="Where to?"
-                label="Destination"
-                slotProps={{
-                  input: {
-                    ...params.InputProps,
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <FlightLandIcon />
-                      </InputAdornment>
-                    ),
-                  },
-                }}
-              />
-            )}
-          />
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker label="Outbound" />
-            <DatePicker label="Inbound" />
-          </LocalizationProvider>
+          <Stack
+            // maxWidth="90%"
+            sx={{
+              justifyContent: 'flex-start',
+              alignItems: { xs: 'flex-start', md: 'flex-start' },
+              flexDirection: { xs: 'row', md: 'row' },
+              mb: { xs: 2, md: 3 },
+              gap: 2,
+              minWidth: { xs: '100%', md: '50%' },
+            }}
+          >
+            <FormControl variant="standard" fullWidth size="small">
+              <Select labelId="demo-simple-select-label" id="demo-simple-select" value="10" label="Age" onChange="">
+                <MenuItem value={10}>Round Trip</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl variant="standard" fullWidth size="small">
+              <Select labelId="demo-simple-select-label1" id="demo-simple-select" value="20" label="Age" onChange="">
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>1</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl variant="standard" fullWidth size="small">
+              <Select labelId="demo-simple-select-label2" id="demo-simple-select" value="30" label="Age" onChange="">
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Economy</MenuItem>
+              </Select>
+            </FormControl>
+          </Stack>
+          <Stack
+            minWidth="50%"
+            maxWidth="90%"
+            sx={{
+              justifyContent: 'flex-start',
+              alignItems: { xs: 'flex-start', md: 'center' },
+              // bgcolor: '#fff',
+              // px: { xs: 3, sm: 5 },
+              // py: { xs: 2, sm: 3 },
+              // borderRadius: '7px',
+              flexDirection: { xs: 'column', md: 'row' },
+              gap: 2,
+            }}
+          >
+            <Autocomplete
+              size="small"
+              disablePortal
+              options={top100Films}
+              sx={{ width: { xs: '70%', sm: 300 } }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  placeholder="Where from?"
+                  label="Origin"
+                  slotProps={{
+                    input: {
+                      ...params.InputProps,
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <FlightTakeoffIcon />
+                        </InputAdornment>
+                      ),
+                    },
+                  }}
+                />
+              )}
+            />
+            <Autocomplete
+              size="small"
+              disablePortal
+              options={top100Films}
+              sx={{ width: { xs: '70%', sm: 300 } }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  placeholder="Where to?"
+                  label="Destination"
+                  slotProps={{
+                    input: {
+                      ...params.InputProps,
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <FlightLandIcon />
+                        </InputAdornment>
+                      ),
+                    },
+                  }}
+                />
+              )}
+            />
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker label="Outbound" slotProps={{ textField: { size: 'small' } }} />
+              <DatePicker label="Inbound" slotProps={{ textField: { size: 'small' } }} />
+            </LocalizationProvider>
+          </Stack>
         </Stack>
         <Button
           variant="outlined"
