e543c6851411c3b07b33746264f302e3b11572c3
Ameera Jayakumar <ameeraj.mec@gmail.com>
2025-02-05 21:11:02 +0200

    datepicker

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,12 @@
-import { useState } from 'react';
-import { Autocomplete, Box, Stack, TextField } from '@mui/material';
+// import { useState } from 'react';
+import { Autocomplete, Box, Button, Stack, TextField } from '@mui/material';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import SearchIcon from '@mui/icons-material/Search';
+import InputAdornment from '@mui/material/InputAdornment';
+import FlightTakeoffIcon from '@mui/icons-material/FlightTakeoff';
+import FlightLandIcon from '@mui/icons-material/FlightLand';
 
 const Search = () => {
   const top100Films = [
@@ -14,31 +21,89 @@ const Search = () => {
   return (
     <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} height="100vh">
       <Stack
-        maxWidth="sm"
-        width="90%"
+        minWidth="50%"
+        maxWidth="90%"
         sx={{
-          justifyContent: 'flex-start',
+          justifyContent: 'center',
           alignItems: 'center',
           bgcolor: '#fff',
           px: { xs: 3, sm: 5 },
           py: { xs: 2, sm: 3 },
           borderRadius: '7px',
-          flexDirection: { xs: 'column', sm: 'row' },
+          flexDirection: 'column',
           gap: 2,
         }}
       >
-        <Autocomplete
-          disablePortal
-          options={top100Films}
-          sx={{ width: { xs: '70%', sm: 300 } }}
-          renderInput={(params) => <TextField {...params} label="Origin" />}
-        />
-        <Autocomplete
-          disablePortal
-          options={top100Films}
-          sx={{ width: { xs: '70%', sm: 300 } }}
-          renderInput={(params) => <TextField {...params} label="Destination" />}
-        />
+        <Stack
+          minWidth="50%"
+          maxWidth="90%"
+          sx={{
+            justifyContent: 'flex-start',
+            alignItems: { xs: 'flex-start', md: 'center' },
+            // bgcolor: '#fff',
+            // px: { xs: 3, sm: 5 },
+            // py: { xs: 2, sm: 3 },
+            // borderRadius: '7px',
+            flexDirection: { xs: 'column', md: 'row' },
+            gap: 2,
+          }}
+        >
+          <Autocomplete
+            disablePortal
+            options={top100Films}
+            sx={{ width: { xs: '70%', sm: 300 } }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                placeholder="Where from?"
+                label="Origin"
+                slotProps={{
+                  input: {
+                    ...params.InputProps,
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <FlightTakeoffIcon />
+                      </InputAdornment>
+                    ),
+                  },
+                }}
+              />
+            )}
+          />
+          <Autocomplete
+            disablePortal
+            options={top100Films}
+            sx={{ width: { xs: '70%', sm: 300 } }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                placeholder="Where to?"
+                label="Destination"
+                slotProps={{
+                  input: {
+                    ...params.InputProps,
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <FlightLandIcon />
+                      </InputAdornment>
+                    ),
+                  },
+                }}
+              />
+            )}
+          />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker label="Outbound" />
+            <DatePicker label="Inbound" />
+          </LocalizationProvider>
+        </Stack>
+        <Button
+          variant="outlined"
+          startIcon={<SearchIcon />}
+          sx={{ color: '#70AAD4', borderColor: '#70AAD4', '&:hover': { borderColor: '#70AAD4', backgroundColor: '#E2F5FC' } }}
+        >
+          Search
+        </Button>
       </Stack>
     </Box>
   );
